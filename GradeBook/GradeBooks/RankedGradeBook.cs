dd73e33a9d2707f6220a337692ef61b54f772947
v-apetrychuk@youngliving.com <andrei.petrychuk@gmail.com>
2019-10-05 10:54:18 +0300

    Work is done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +7,31 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade < (int)Math.Ceiling(Students.Count* 0.2))
+            {
+                return 'A';
+            }
+            else if (averageGrade < (int)Math.Ceiling(Students.Count * 0.4))
+            {
+                return 'B';
+            }
+            else if (averageGrade < (int)Math.Ceiling(Students.Count * 0.6))
+            {
+                return 'C';
+            }
+            else if (averageGrade < (int)Math.Ceiling(Students.Count * 0.8))
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
